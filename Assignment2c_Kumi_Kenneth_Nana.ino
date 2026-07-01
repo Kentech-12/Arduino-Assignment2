d92ca1204dd6918dd28060c16410d311570f9790
Kentech-12 <kentech856@gmail.com>
2026-07-01 13:40:42 +0000

    Add files via upload
    Add Task 5 - beeping countdown sketch

---
+++ Assignment2c_Kumi_Kenneth_Nana.ino
@@ -0,0 +1,50 @@
+int segmentPins[] = {2, 3, 4, 5, 6, 7, 9};
+int numSegments = 7;
+int buzzerPin = 8;
+byte digits[10][7] ={
+  {1, 1, 1, 1, 1, 1, 0}, // 0
+  {0, 1, 1, 0, 0, 0 ,0}, // 1
+  {1, 1, 0, 1, 1, 0, 1}, // 2
+  {1, 1, 1, 1, 0, 0, 1}, // 3
+  {0, 1, 1, 0, 0, 1, 1}, // 4
+  {1, 0, 1, 1, 0, 1, 1}, // 5
+  {1, 0, 1, 1, 1, 1, 1}, // 6
+  {1, 1, 1, 0, 0, 0, 0}, // 7
+  {1, 1, 1, 1, 1, 1, 1}, // 8
+  {1, 1, 1, 1, 0, 1, 1}, // 9
+};
+
+void showDigit(int n){
+   if (n < 0 || n > 9)  return;
+
+  for(int i = 0 ; i < numSegments; i++){
+    digitalWrite(segmentPins[i], digits[n][i]);
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  for(int i = 0; i < 9; i++){
+    pinMode(segmentPins[i], OUTPUT);
+  }
+  Serial.begin(9600);
+  Serial.println("===Beeping Countdown Starting===");
+  int count = 9;
+  while(count >= 0){
+    Serial.print("Counting: ");
+    Serial.println(count);
+    showDigit(count);
+    tone(buzzerPin, 1000,200);
+    delay(1000);
+    count = count -1;
+  }
+  tone(buzzerPin, 1500, 1000);
+  Serial.print("===Countdown Complete ===");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
